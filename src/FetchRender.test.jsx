c7d598fa402bf2743b4e4dd2d986bf601b561791
Manoj Mukherjee <manojmukherjee777@gmail.com>
2024-02-18 13:25:10 +0530

    added unit test

--- src/FetchRender.test.jsx
+++ src/FetchRender.test.jsx
@@ -1,24 +1,48 @@
-import { render, screen, userEvent, waitFor, debug } from './utils/test-utils';
-import { http, HttpResponse } from 'msw';
-import { setupServer } from 'msw/node'
-import FetchRenderTest from './FetchRenderTest';
+import {
+	render,
+	screen,
+	userEvent,
+	waitFor,
+	debug,
+	act,
+} from "./utils/test-utils";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import FetchRenderTest from "./FetchRenderTest";
 
 const handlers = [
-    http.put('https://65d1ac70987977636bfb57a9.mockapi.io/api/v1/test', () => {
-        return HttpResponse.json({"msg":"Invalid request"}, {status: 400}) // Simulate error response
-    })]
+	http.put("https://65d1ac70987977636bfb57a9.mockapi.io/api/v1/test", () => {
+		return HttpResponse.json({ msg: "Invalid request" }, { status: 400 }); // Simulate error response
+	}),
+];
 const server = setupServer(...handlers);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('renders ErrorModal when fetch fails', async () => {
-    render(<FetchRenderTest />);
+test("renders ErrorModal when fetch fails", async () => {
+	render(<FetchRenderTest />, { container: document.body });
 
-    const testingStartButton = screen.getByText('Testing Start');
+	const testingStartButton = screen.getByText("Testing Start");
+	await act(async () => {
+		// Click the button to trigger the fetch request
+		await userEvent.click(testingStartButton);
+	});
 
-    // Click the button to trigger the fetch request
-    await userEvent.click(testingStartButton);
-    
+	// Wait for the ErrorModal to render
+	await waitFor(() => {
+		const errorModal = screen.getByRole("dialog");
+		expect(errorModal).toBeInTheDocument();
+	});
+
+	// Verify the modal content
+	const modalTitle = screen.getByText("Modal, Dialog Test");
+	expect(modalTitle).toBeInTheDocument();
+
+	// Verify the close button is rendered
+	const closeButton = screen.getByRole("button", {
+		name: /Error dialog close/i,
+	});
+	expect(closeButton).toBeInTheDocument();
 });
--- src/UI/ErrorModel.jsx
+++ src/UI/ErrorModel.jsx
@@ -1,46 +1,58 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import classes from './ErrorModal.module.css';
+import React, { Fragment, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import classes from "./ErrorModal.module.css";
 
 const ErrorModalOverlay = (props) => {
-  return (
-    <div className={classes.backdrop} data-testid="model-dialog">
-      <div className={classes.container}>
-        <section>
-          <header className={classes.header}>
-            <h2>{props.title}</h2>
-          </header>
-          <div className={classes.content}>
-            <p>{props.message}</p>
-          </div>
-          <footer className={classes.footer}>
-            <button className={classes.button} onClick={props.isConfirm}>
-              Error dialog close
-            </button>
-          </footer>
-        </section>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classes.backdrop} role="dialog">
+			<div className={classes.container}>
+				<section>
+					<header className={classes.header}>
+						<h2>{props.title}</h2>
+					</header>
+					<div className={classes.content}>
+						<p>{props.message}</p>
+					</div>
+					<footer className={classes.footer}>
+						<button className={classes.button} onClick={props.isConfirm}>
+							Error dialog close
+						</button>
+					</footer>
+				</section>
+			</div>
+		</div>
+	);
 };
-
-const portalElement = document.getElementById('overlays');
 const ErrorModal = (props) => {
-  const { title, message, onConfirm } = props;
-  if (!portalElement) {
-    return null;
-  }
-  const messageText = message ? message.result : null;
-  const errorModalElements = ReactDOM.createPortal(
-    <ErrorModalOverlay
-      title={title}
-      message={messageText}
-      isConfirm={onConfirm}
-    ></ErrorModalOverlay>,
-    portalElement
-  );
+	const modalRoot = useRef(document.createElement("div"));
+	useEffect(() => {
+		let root = document.getElementById("overlays");
+		/**
+		 * this is for testing
+		 */
+		if (!root) {
+			root = document.createElement("div");
+			root.setAttribute("id", "overlays");
+			document.body.appendChild(root);
+		}
 
-  return <Fragment>{errorModalElements}</Fragment>;
+		root.appendChild(modalRoot.current);
+
+		return () => {
+			root.removeChild(modalRoot.current);
+		};
+	}, []);
+	const { title, message, onConfirm } = props;
+	const messageText = message ? message.result : null;
+	const errorModalElements = ReactDOM.createPortal(
+		<ErrorModalOverlay
+			title={title}
+			message={messageText}
+			isConfirm={onConfirm}></ErrorModalOverlay>,
+		modalRoot.current
+	);
+
+	return <Fragment>{errorModalElements}</Fragment>;
 };
 
 export default ErrorModal;
